dc940811dcec13f002acd387647518e72af07eee
Mateo Bertello <mateo.bertello@gmail.com>
2025-03-09 23:20:50 -0300

    Add internationalization support; implement translations for profile, skills, projects, and footer components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    i18n: {
+        defaultLocale: 'es',
+        locales: ['es', 'en'],
+        routing: {
+            prefixDefaultLocale: false,
+            redirectToDefaultLocale: true,
+        },
+    },
     integrations: [react()],
 });
