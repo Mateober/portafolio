1395209851515ecaa65a34289dd22f0ad539f780
Mateo Bertello <mateo.bertello@gmail.com>
2025-03-21 20:09:30 -0300

    Add skill section styles and update theme variables; enhance icon colors and background for improved UI

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -14,7 +14,7 @@ const Languages = (translations) => {
     const onClickLanguage = (lang) => {
         if (typeof window !== "undefined") {
             const newPath = lang === "es" ? "/" : "/en";
-            window.location.pathname = newPath;
+            window.location.pathname = newPath; 
         }
     };
 
@@ -22,7 +22,7 @@ const Languages = (translations) => {
         <div className="idioma-container">
             <div className="btn-language" onClick={() => setOpen(!open)}>
                 <p className="principal">{translations?.navbar?.idioma}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <svg className="arrow-language" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m6 9l6 6l6-6"></path>
                 </svg>
             </div>
@@ -31,7 +31,7 @@ const Languages = (translations) => {
                 <div className="idioma" onClick={() => onClickLanguage("es")}>
                     <p>Español</p>
                     {currentLanguage === "es" && (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <svg  className="navbariconcolor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path fill="#fff" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z"></path>
                         </svg>
                     )}
@@ -39,7 +39,7 @@ const Languages = (translations) => {
                 <div className="idioma" onClick={() => onClickLanguage("en")}>
                     <p>English</p>
                     {currentLanguage === "en" && (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <svg className="navbariconcolor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path fill="#fff" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z"></path>
                         </svg>
                     )}
--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -27,7 +27,7 @@ export default function Theme() {
 }
 
 const SunIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <svg className='' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <g fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
             <circle cx="12" cy="12" r="4"></circle>
             <path d="M12 3v1m0 16v1m-9-9h1m16 0h1m-2.636-6.364l-.707.707M6.343 17.657l-.707.707m0-12.728l.707.707m11.314 11.314l.707.707"></path>
@@ -36,7 +36,7 @@ const SunIcon = () => (
 );
 
 const MoonIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <svg className='moon-icon' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3a6 6 0 0 0 9 9a9 9 0 1 1-9-9"></path>
     </svg>
 );
