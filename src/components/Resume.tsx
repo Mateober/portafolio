064a0b6585c7f9b0ac027f9a109bffd6d828394d
Mateo Bertello <mateo.bertello@gmail.com>
2025-04-14 11:20:13 -0300

    Refactor Resume component; update SVG classes for better styling and add SCSS rules for icon colors.

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -7,7 +7,7 @@ const Resume = (translations) => {
     return (
         <div className="resume-container">
             <div className="btn-resume" onClick={() => setOpen(!open)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20">
+                <svg className='resume-icon3' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20">
                     <g fill="none">
                         <path fill="#fff" d="M6.5 12.5a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zm0 2.5a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1z" />
                         <path
@@ -16,7 +16,7 @@ const Resume = (translations) => {
                             d="M11.185 1H4.5A1.5 1.5 0 0 0 3 2.5v15A1.5 1.5 0 0 0 4.5 19h11a1.5 1.5 0 0 0 1.5-1.5V7.202a1.5 1.5 0 0 0-.395-1.014l-4.314-4.702A1.5 1.5 0 0 0 11.185 1M4 2.5a.5.5 0 0 1 .5-.5h6.685a.5.5 0 0 1 .369.162l4.314 4.702a.5.5 0 0 1 .132.338V17.5a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5z"
                             clip-rule="evenodd"
                         />
-                        <path stroke="#fff" stroke-linecap="round" stroke-linejoin="round" d="M11.5 2.1v4.7h4.7" stroke-width="1" />
+                        <path className='stroke-cv' stroke="#fff" stroke-linecap="round" stroke-linejoin="round" d="M11.5 2.1v4.7h4.7" stroke-width="1" />
                         <path fill="#fff" d="M8.134 6.133a1.067 1.067 0 1 0 0-2.133a1.067 1.067 0 0 0 0 2.133" />
                         <path
                             fill="#fff"
@@ -34,16 +34,16 @@ const Resume = (translations) => {
 
             <div className={`resumes ${open ? 'open' : ''}`}>
                 <a href={translations?.navbar?.cvUrl} target="_blank" rel="noopener noreferrer" className="resume">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <svg className='resume-icon' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <g fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
                             <path d="M2.062 12.348a1 1 0 0 1 0-.696a10.75 10.75 0 0 1 19.876 0a1 1 0 0 1 0 .696a10.75 10.75 0 0 1-19.876 0" />
-                            <circle cx="12" cy="12" r="3" />
+                            <circle cx="12" cy="12" r="3"/>
                         </g>
                     </svg>
                     <p>{translations?.navbar?.cvView}</p>
                 </a>
                 <a href={translations?.navbar?.cvUrl} download className="resume">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <svg className='resume-icon' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4m4-5l5 5l5-5m-5 5V3" />
                     </svg>
                     <p>{translations?.navbar?.cvDownload}</p>
